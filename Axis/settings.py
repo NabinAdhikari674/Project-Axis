d0bb9d50220e2b89dced12bfb8be10c8670fddf5
NabinAdhikari674 <nabinadhikari674@gmail.com>
2020-07-27 12:10:08 +0530

    Patch

--- Axis/settings.py
+++ Axis/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'saw=1)(50q04_!_m6qf!s6^o8ldm06-%kwpzszt$iqc+$+iv13'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['project-axis.herokuapp.com/']
 
