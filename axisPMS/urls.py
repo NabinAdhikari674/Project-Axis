1777d762bd6204b60d951c88f2d9199f77c0abf7
gautam591 <gautambohara591@gmail.com>
2020-08-01 00:59:15 +0900

    i update the urls  now i will add on views section

--- axisPMS/urls.py
+++ axisPMS/urls.py
@@ -4,6 +4,14 @@ from django.urls import path
 app_name = 'axisPMS' # NameSpace for app
 
 urlpatterns = [
-    #path('', views.postView, name='postView'),
-    #path('postDetail/', views.postDetail, name='postDetail'),
+   # path('', views.pmsPostView, name='pmspostView'),
+    #path('sortPost/', views.earliestPost, name='earliestPost'),
+    #path('oldest/', views.oldestpost, name='oldestPost'),
+    #path('earliest/', views.earliestpost, name='earliestPost'),
+    #path('latest/', views.latestPost, name='latestPost'),
+    #path('government/',views.governmentPost,name='governmentPost'),
+    #path ('private/',views.privatePost,name='privatePost'),
+    #path('tender/',views.tender,name='tender),
+
+
 ]
